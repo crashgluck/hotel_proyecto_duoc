76ba4d97961579ce7df9ad56230754a13dfd874a
crashgluck <cris.vera.olivares@gmail.com>
2025-09-12 17:45:02 -0300

    Se agegaron estilos y componentes al landing

--- hotel/settings.py
+++ hotel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-egy@fi%_t(47s78p&qbg@$_y@+m=+q2e8cm0x#_8iz+ruym&1z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
